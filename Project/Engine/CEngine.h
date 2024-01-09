b103fa9d6cbca5de7bbd966739713265058b3c6c
Gihoon <kih0976@gmail.com>
2024-01-10 01:04:38 +0900

    device, context, swapchain 분리 완료

--- Project/Engine/CEngine.h
+++ Project/Engine/CEngine.h
@@ -7,27 +7,10 @@ class CEngine
 
 private:
 	HWND				m_hWnd;
-	unsigned __int32	m_width;
-	unsigned __int32	m_height;
-	bool				m_bStandByMode = false;
-
-private:
-	//ComPtr<ID3D11Device>			m_device;
-	//ComPtr<ID3D11DeviceContext>		m_context;
-	//ComPtr<IDXGISwapChain>			m_swapChain;
-	//ComPtr<ID3D11RenderTargetView>	m_renderTargetView;
-	//D3D11_VIEWPORT					m_viewport;
 
 public:
 	int Init(HWND _hWnd);
 	void Update();
 	void Render();
-
-private:
-	void RenderBegin();
-	void RenderEnd();
-	void DeviceAndSwapChain();
-	void RenderTargetView();
-	void SetViewport();
 };
 
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ DirectX 2D Engine
 **_24/01/09_** :<br>
 &nbsp;&nbsp;- Unripe 엔진 초기화<br>
 &nbsp;&nbsp;- type 재정의 (types.h)<br>
-&nbsp;&nbsp;- <br>
+&nbsp;&nbsp;- Device, Context, SwapChain 분리 (CGraphics)<br>
